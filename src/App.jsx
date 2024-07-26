82439d14e33b3b1bfa4c0b2834bff5578b2f8b77
Tejas <kumarsid795@gmail.com>
2024-07-27 00:01:10 +0530

    "first commit"

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Highlights from './components/Highlights.jsx';
+import Model from './components/Model.jsx'
+import Features from './components/Features.jsx';
+import Chip from './components/Chip.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <Chip />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
